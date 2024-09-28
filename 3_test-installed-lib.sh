c7e30cd64638d1d1b586a9afe232bdc036e861c4
Tamas Kenez <tamas.kenez@gmail.com>
2024-09-28 15:58:43 +0200

    fix

--- 3_test-installed-lib.sh
+++ 3_test-installed-lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-cmake -S test-installed-lib -B bdt -D CMAKE_BUILD_TYPE=Debug -D CMAKE_PREFIX_PATH=${PWD}/i
-cmake -S test-installed-lib -B brt -D CMAKE_BUILD_TYPE=Release -D CMAKE_PREFIX_PATH=${PWD}/i
+cmake -S test-installed-lib -B bdt -D CMAKE_BUILD_TYPE=Debug -D "CMAKE_PREFIX_PATH=${PWD}/i;${PWD}/id"
+cmake -S test-installed-lib -B brt -D CMAKE_BUILD_TYPE=Release -D "CMAKE_PREFIX_PATH=${PWD}/i;${PWD}/id"
 cmake --build bdt --config Debug -j
 cmake --build brt --config Release -j
 ctest -C Debug --test-dir bdt -V
