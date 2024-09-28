eedc8b9abd7556c931d7cd8bf619f553e05f440c
Tamas Kenez <tamas.kenez@gmail.com>
2024-09-28 16:57:03 +0200

    works

--- 3_test-installed-lib.sh
+++ 3_test-installed-lib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-cmake -S test-installed-lib -B bdt -D CMAKE_BUILD_TYPE=Debug   -D "CMAKE_PREFIX_PATH=../i;../id" --debug-find-pkg=sqlitecpp-thin
+cmake -S test-installed-lib -B bdt -D CMAKE_BUILD_TYPE=Debug   -D "CMAKE_PREFIX_PATH=../i;../id"
 cmake -S test-installed-lib -B brt -D CMAKE_BUILD_TYPE=Release -D "CMAKE_PREFIX_PATH=../i;../id"
 cmake --build bdt --config Debug -j
 cmake --build brt --config Release -j
