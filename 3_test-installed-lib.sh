89963b876dd31898095b36d0331d37f7933aaa63
Tamas Kenez <tamas.kenez@gmail.com>
2024-09-28 16:52:34 +0200

    maybe windows fix

--- 3_test-installed-lib.sh
+++ 3_test-installed-lib.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
-cmake -S test-installed-lib -B bdt -D CMAKE_BUILD_TYPE=Debug -D "CMAKE_PREFIX_PATH=${PWD}/i;${PWD}/id" --debug-find-pkg=sqlitecpp-thin
-cmake -S test-installed-lib -B brt -D CMAKE_BUILD_TYPE=Release -D "CMAKE_PREFIX_PATH=${PWD}/i;${PWD}/id"
+cmake -S test-installed-lib -B bdt -D CMAKE_BUILD_TYPE=Debug   -D "CMAKE_PREFIX_PATH=../i;../id" --debug-find-pkg=sqlitecpp-thin
+cmake -S test-installed-lib -B brt -D CMAKE_BUILD_TYPE=Release -D "CMAKE_PREFIX_PATH=../i;../id"
 cmake --build bdt --config Debug -j
 cmake --build brt --config Release -j
 ctest -C Debug --test-dir bdt -V
