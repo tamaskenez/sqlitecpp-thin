9926cd903539430595aee683d8d4c295542c5d41
Tamas Kenez <tamas.kenez@gmail.com>
2024-10-21 00:17:44 +0200

    Move conan detection to workflow

--- 1_build_conan_deps.sh
+++ 1_build_conan_deps.sh
@@ -6,7 +6,6 @@ if [[ $(conan --version) == *" 1."* ]]; then
 	conan install conanfile.txt -b missing -pr:b default -if id/cmake -of id/cmake -s build_type=Debug
 	conan install conanfile.txt -b missing -pr:b default -if id/cmake -of id/cmake -s build_type=Release
 else
-	conan profile detect
 	conan install conanfile.txt -b missing -pr:b default -of id/cmake -s build_type=Debug
 	conan install conanfile.txt -b missing -pr:b default -of id/cmake -s build_type=Release
 fi
